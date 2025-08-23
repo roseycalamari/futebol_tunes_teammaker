f8591d9332b094dc52f14903f6f7248590cde856
roseycalamari <rosaruben7@gmail.com>
2025-08-23 22:18:29 +0100

    m

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration file for API keys and settings
 const CONFIG = {
     RESEND_API_KEY: 're_FQXVh4R4_7Ji9SEyB7HHyi8SsKsd6ZCRk',
-    FROM_EMAIL: 'NiteRun Team <niterun@updates.resend.dev>',
+    FROM_EMAIL: 'NiteRun Team <onboarding@resend.dev>',
     APP_URL: 'https://futeboltunesteammaker.vercel.app'
 };
 
